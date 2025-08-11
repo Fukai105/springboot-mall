28238098347d54c0b45a2a6df94c9a9436e6af40
Fukai105 <a0900235850@gmial.com>
2025-08-11 15:10:36 +0800

    新增商品

--- springboot-mall/src/main/java/com/example/springbootmall/dao/ProductDao.java
+++ springboot-mall/src/main/java/com/example/springbootmall/dao/ProductDao.java
@@ -1,5 +1,6 @@
 package com.example.springbootmall.dao;
 
+import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +8,6 @@ import org.springframework.stereotype.Component;
 public interface ProductDao {
 
     Product getProductById(Integer productId);
+
+    Integer createProduct(ProductRequest productRequest);
 }
--- springboot-mall/src/main/java/com/example/springbootmall/service/ProductService.java
+++ springboot-mall/src/main/java/com/example/springbootmall/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.example.springbootmall.service;
 
+import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 import org.springframework.stereotype.Component;
 
@@ -7,4 +8,5 @@ import org.springframework.stereotype.Component;
 public interface ProductService  {
    Product getProductById(Integer productId);
 
+   Integer createProduct(ProductRequest productRequest);
 }
