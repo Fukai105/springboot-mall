506b739294f450e4641a0f0126b5fd1c04b097a7
Fukai105 <a0900235850@gmial.com>
2025-08-11 13:37:25 +0800

    查詢商品-Enum

--- springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.springbootmall.controller;
 import com.example.springbootmall.model.Product;
 import com.example.springbootmall.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
