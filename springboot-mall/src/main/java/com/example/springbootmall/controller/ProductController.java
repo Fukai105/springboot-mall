7b340cea481655479ea2f6dc58fef29d771f8ae7
Fukai105 <a0900235850@gmial.com>
2025-08-12 10:59:57 +0800

    查詢條件分頁商品列表

--- springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
+++ springboot-mall/src/main/java/com/example/springbootmall/controller/ProductController.java
@@ -6,6 +6,8 @@ import com.example.springbootmall.dto.ProductRequest;
 import com.example.springbootmall.model.Product;
 import com.example.springbootmall.service.ProductService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 public class ProductController {
 
@@ -22,17 +25,24 @@ public class ProductController {
 
     @GetMapping("products")
     public  ResponseEntity<List<Product>> getProducts(
+            //查詢條件Filtering
             @RequestParam (required = false)ProductCategory category,
             @RequestParam (required = false)String search,
+            //排序sorting
             @RequestParam (defaultValue = "created_date")String orderBy,
-            @RequestParam (defaultValue = "desc")String sort
-            ){
+            @RequestParam (defaultValue = "desc")String sort,
+            //分頁Pagination
+            @RequestParam (defaultValue = "5") @Max(1000) @Min(0) Integer limit,
+            @RequestParam (defaultValue = "0") @Min(0) Integer offset
+    ){
 
         ProductQueryParams productQueryParams = new ProductQueryParams();
         productQueryParams.setCategory(category);
         productQueryParams.setSearch(search);
         productQueryParams.setOrderBy(orderBy);
         productQueryParams.setSort(sort);
+        productQueryParams.setLimit(limit);
+        productQueryParams.setOffset(offset);
 
         List<Product> productList=productService.getProducts(productQueryParams);
         return new ResponseEntity<>(productList,HttpStatus.OK);
